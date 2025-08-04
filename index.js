aa0843a2735306501601b09559342c0b9138954f
AvinashNagula <111056066+AvinashNagula@users.noreply.github.com>
2025-08-04 11:54:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// index.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// Root route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
